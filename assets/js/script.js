9cccd966ebe236beff3fc636344dcdc79a135ba6
Joelle DesChatelets <jdeschat@gmail.com>
2021-07-26 19:31:47 -0400

    finished preset City Buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,28 +22,30 @@ searchButton.addEventListener("click", function () {
 })
 
 // TODO: get the preset buttons to return weather information
-var presetCityButtons = document.querySelector(".cityNames");
-presetCityButtons.addEventListener("click", function (e) {
-    let cityName = $("#cityInput").val();
-    let cityNames = e.target.innerText;
-    // console.log(cityName);
-    let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=908d66bc443a59edcf38648405a06695";
-    fetch(cityNames)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            if (data.cod !== "200") {
-                console.log("City not found. Please try again");
-                return;
-            }
-            // Use 'querySelector' to get the ID of where the Search for a City will be displayed
-            var responseContainerEl = document.querySelector("#response-container");
-            console.log(data)
-        })
-        .catch(err => console.log(err));
-
+var presetCityButtons = document.querySelectorAll(".cityNames");
+presetCityButtons.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        console.log(document.querySelectorAll(".cityNames"));
+        let cityName = e.target.innerText;
+        // console.log(cityName);
+        let weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=908d66bc443a59edcf38648405a06695";
+        fetch(weatherAPI)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                if (data.cod !== "200") {
+                    console.log("City not found. Please try again");
+                    return;
+                }
+                // Use 'querySelector' to get the ID of where the Search for a City will be displayed
+                var responseContainerEl = document.querySelector("#response-container");
+                console.log(data)
+            })
+            .catch(err => console.log(err));
+    });
 });
+
 
 // TODO: Create a container that contains the city, date, temp, wind, humidity and UV index
 var jsonData;
